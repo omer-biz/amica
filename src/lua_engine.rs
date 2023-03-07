c08539b3fbed6449cbe0cd696945b6bd0db4d7a6
omer <omerabdi@protonmail.com>
2023-03-07 21:45:30 +0300

    propagate lua errors.

--- src/lua_engine.rs
+++ src/lua_engine.rs
@@ -12,51 +12,35 @@ impl LuaEngine {
         LuaEngine { lua_vm: Lua::new() }
     }
 
-    pub(crate) fn load(&self, lua_code: &str) -> Result<(), ()> {
+    pub(crate) fn load(&self, lua_code: &str) -> rlua::Result<()> {
         self.lua_vm.context(|lua_context| {
-            let a = lua_context.load(lua_code).eval::<MultiValue>();
-            if a.is_err() {
-                println!("{}", a.unwrap_err());
-                return Err(());
-            }
+            lua_context.load(lua_code).eval::<MultiValue>()?;
             Ok(())
         })?;
 
         Ok(())
     }
 
-    fn call_lua_function<A, R>(&self, function: &str, args: A) -> Result<R, ()>
+    fn call_lua_function<A, R>(&self, function: &str, args: A) -> rlua::Result<R>
     where
         A: for<'lua> ToLuaMulti<'lua>,
         R: for<'lua> FromLuaMulti<'lua>,
     {
         self.lua_vm.context(move |lua_context| {
-            let lua_function = lua_context.globals().get::<_, Function>(function);
-            if lua_function.is_err() {
-                eprintln!("{}", lua_function.unwrap_err());
-                return Err(());
-            }
-            let lua_function = lua_function.unwrap();
+            let lua_function = lua_context.globals().get::<_, Function>(function)?;
+            let lua_result = lua_function.call::<A, R>(args)?;
 
-            let lua_result = lua_function.call::<A, R>(args);
-
-            if lua_result.is_err() {
-                return Err(());
-            }
-
-            Ok(lua_result.unwrap())
+            Ok(lua_result)
         })
     }
 
-    pub(crate) fn call_on_http_request(&self, req: ProxyRequest) -> Result<Request<Body>, ()> {
-        let r = self.call_lua_function::<_, ProxyRequest>("on_http_request", req);
-
-        Ok(r.unwrap().into())
+    pub(crate) fn call_on_http_request(&self, req: ProxyRequest) -> rlua::Result<Request<Body>> {
+        let request = self.call_lua_function::<_, ProxyRequest>("on_http_request", req)?;
+        Ok(request.into())
     }
 
-    pub(crate) fn call_on_http_response(&self, res: ProxyResponse) -> Result<Response<Body>, ()> {
-        let r = self.call_lua_function::<_, ProxyResponse>("on_http_response", res);
-
-        Ok(r.unwrap().into())
+    pub(crate) fn call_on_http_response(&self, res: ProxyResponse) -> rlua::Result<Response<Body>> {
+        let response = self.call_lua_function::<_, ProxyResponse>("on_http_response", res)?;
+        Ok(response.into())
     }
 }
