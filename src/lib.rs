4e004d6a96ca6f8803bfe32ddcd09c00f491e88b
omer <omerabdi@protonmail.com>
2023-03-13 20:51:08 +0300

    error handling with anyhow, move to own module.

--- src/lib.rs
+++ src/lib.rs
@@ -1,83 +1,119 @@
+mod intermediate_proxy_data;
 mod lua_engine;
-mod proxy;
 
+use anyhow::Context;
+use clap::Parser;
 use hyper::{server::conn::Http, service::service_fn, Body, Request, Response};
+use std::path::PathBuf;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpStream,
+    net::{TcpListener, TcpStream},
 };
 
+use intermediate_proxy_data::{ProxyRequest, ProxyResponse};
 use lua_engine::LuaEngine;
-use proxy::{ProxyRequest, ProxyResponse};
 
-// struct ProxyHandler;
+static mut LUA_CODE: Option<String> = None;
 
-pub async fn handle_client(mut client: TcpStream) {
-    let mut buf = [0; 512];
-    client.peek(&mut buf).await.unwrap();
+#[derive(Parser)]
+pub struct Args {
+    /// Optinal lua script to run on the the intermediate
+    /// request and response.
+    #[arg(short, long, value_name = "lua script")]
+    filter_script: Option<PathBuf>,
 
-    if buf.starts_with(&[67, 79, 78, 78, 69, 67, 84]) {
-        let nbytes = client.read(&mut buf).await.unwrap();
+    /// Address to bind to.
+    #[arg(short, long, value_name = "ip:port")]
+    address: String,
+}
 
-        let head = String::from_utf8_lossy(&buf[..nbytes]);
-        let host = head.split_whitespace().nth(1).unwrap();
+pub struct Proxy {
+    args: Args,
+}
 
+impl Proxy {
+    pub async fn start(args: Args) -> anyhow::Result<()> {
+        Proxy { args }.run().await
+    }
 
-        client.write_all(b"HTTP/1.1 200 OK\r\n\r\n").await.unwrap();
+    async fn run(&self) -> anyhow::Result<()> {
+        let tcp_listener = TcpListener::bind(&self.args.address)
+            .await
+            .with_context(|| format!("Can not bind to {}", self.args.address))?;
+        println!("Listening on {}", self.args.address);
 
-    } else {
-        let _ = Http::new()
-            .serve_connection(client, service_fn(handle_http_request))
-            .await;
+        loop {
+            let (client, sock_addr) = tcp_listener.accept().await?;
+            println!("client connected on: {}", sock_addr);
+
+            if let Some(path) = &self.args.filter_script {
+                unsafe { LUA_CODE = Some(std::fs::read_to_string(path)?) }
+            }
+            tokio::spawn(async move {
+                if let Err(error) = handle_client(client).await {
+                    eprintln!("Error: {}", error);
+                }
+            });
+        }
     }
 }
 
+async fn handle_client(mut client: TcpStream) -> anyhow::Result<()> {
+    let mut buf = [0; 1024];
+    let nbytes = client.peek(&mut buf).await?;
 
-async fn handle_http_request(request: Request<Body>) -> Result<Response<Body>, String> {
-    let lua_req_filter = r#"
-    function on_http_request(req)
-        print("request: ", req:uri())
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut req = httparse::Request::new(&mut headers);
+    let _req_size = req.parse(&buf[..nbytes])?;
 
+    let method = req
+        .method
+        .with_context(|| "Can't find the `Method` of the request")?
+        .to_string();
 
-        for k, v in pairs(req:headers()) do
-           print(k, v)
-        end
+    let host = headers
+        .iter()
+        .find(|header| header.name == "Host")
+        .with_context(|| "Can't find `Host` in the request header")?;
 
-        return req
-    end
+    println!("host: {}", String::from_utf8_lossy(host.value));
 
-    function on_http_response(res)
-        print("body:", res:body())
+    if method == "CONNECT" {
+        let mut server =
+            TcpStream::connect(String::from_utf8_lossy(host.value).to_string()).await?;
+        client.read(&mut buf).await?;
+        client.write_all(b"HTTP/1.1 200 OK\r\n\r\n").await?;
+        tokio::io::copy_bidirectional(&mut server, &mut client).await?;
+    } else {
+        Http::new()
+            .serve_connection(client, service_fn(handle_http_request))
+            .await?;
+    }
 
-        for k, v in pairs(res:headers()) do
-           print(k .. ": " .. v)
-        end
+    Ok(())
+}
 
-        res:set_body("<h1>This is from the prox</h1>\n"..res:body())
-        headers = res:headers()
-        headers["content-length"] = 63
-        res:set_headers(headers)
+async fn handle_http_request(request: Request<Body>) -> anyhow::Result<Response<Body>> {
+    let no_lua_vm = unsafe { LUA_CODE.is_none() };
 
-        print("")
-        for k, v in pairs(res:headers()) do
-           print(k .. ": " .. v)
-        end
+    let response = if no_lua_vm {
+        make_http_request(request).await?
+    } else {
+        let lua_engine = LuaEngine::new();
+        unsafe { lua_engine.load(LUA_CODE.as_ref().expect(""))? }
 
-        return res
-    end
-    "#;
+        let proxy_request = ProxyRequest::from(request).await?;
+        let request = lua_engine.call_on_http_request(proxy_request)?;
 
-    let lua_engine = LuaEngine::new();
-    lua_engine.load(lua_req_filter).unwrap();
+        let response = make_http_request(request).await?;
 
-    let proxy_request = ProxyRequest::from(request).await;
-    let req = lua_engine.call_on_http_request(proxy_request).unwrap();
+        let proxy_response = ProxyResponse::from(response).await?;
+        lua_engine.call_on_http_response(proxy_response)?
+    };
 
-    let hyper_client = hyper::Client::new();
-    let response = hyper_client.request(req).await.unwrap();
+    Ok::<_, anyhow::Error>(response)
+}
 
-    let proxy_response = ProxyResponse::from(response).await;
-    let response = lua_engine.call_on_http_response(proxy_response).unwrap();
-
-    Ok::<_, String>(response)
+async fn make_http_request(request: Request<Body>) -> hyper::Result<Response<Body>> {
+    hyper::Client::new().request(request).await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
-use amica::handle_client;
-use tokio::net::TcpListener;
+use amica::{Args, Proxy};
+use clap::Parser;
 
 #[tokio::main]
-async fn main() {
-    let address = "127.0.0.1:9001";
-    let tcp_listener = TcpListener::bind(address).await.unwrap();
-    println!("listening on {}", address);
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    Proxy::start(args).await?;
 
-    loop {
-        let (client_tcp_stream, _sock_addr) = tcp_listener.accept().await.unwrap();
-        tokio::spawn(async move {
-            handle_client(client_tcp_stream).await;
-        });
-    }
+    Ok(())
 }
